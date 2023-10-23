fef7aa6dae5ea7d8c3a098115ce196836cf725c6
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-10-23 15:05:58 +0330

    deleted

--- src/featured/Featured.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import styles from "./Featured.module.css";
-const Featured = () => {
-  return <div className={styles.container}>Featured</div>;
-};
-
-export default Featured;
