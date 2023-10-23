02cbb9719021e7371fb02aa1fe7c84b66e5aca33
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-10-23 15:02:04 +0330

    moved to components

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,8 @@
-import Link from "next/link";
+import Featured from "@/components/featured/Featured";
 import styles from "./homepage.module.css";
 import CategoryList from "@/components/categoryList/CategoryList";
 import CardList from "@/components/cardList/CardList";
-import Menu from "@/menu/Menu";
-import Featured from "@/featured/Featured";
+import Menu from "@/components/menu/Menu";
 
 export default function Home() {
   return (
