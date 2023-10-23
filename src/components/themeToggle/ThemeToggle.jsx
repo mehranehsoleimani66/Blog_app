e982234daa1b4481d5fdf0413587c982a3e1794e
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-10-23 10:42:47 +0330

    added themetoggle.jsx and css

---
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -0,0 +1,14 @@
+import styles from "./ThemeToggle.module.css";
+import Image from "next/image";
+const ThemeToggle = () => {
+  return (
+    <div className={styles.container}>
+      <Image src="/moon.png" width={14} height={14} />
+      <div className={styles.ball}></div>
+      <Image src="/sun.png" width={14} height={14} />
+      <Image />
+    </div>
+  );
+};
+
+export default ThemeToggle;
