1c63cbbf043c851e0a70ba6261f5d1a6e0fcbbb2
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-10-24 18:30:48 +0330

    expaned cartlist.jsx

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import styles from "./CardList.module.css";
 import Pagination from "@/components/pagination/Pagination";
+
+import Card from "../card/Card";
 const CardList = () => {
   return (
     <div className={styles.container}>
+      <h1 className={styles.title}>Resent Post</h1>
+      <div className={styles.posts}>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
       <Pagination />
     </div>
   );
