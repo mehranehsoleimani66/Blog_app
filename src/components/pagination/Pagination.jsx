27bd39697506c0721be94b1dd7762d285b414c3a
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-10-26 18:16:08 +0330

    added pagination component  to homepage

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import styles from "./Pagination.module.css";
 const Pagination = () => {
-  return <div className={styles.container}>Pagination</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.button}>previous</div>
+      <div className={styles.button}>next</div>
+    </div>
+  );
 };
 
 export default Pagination;
