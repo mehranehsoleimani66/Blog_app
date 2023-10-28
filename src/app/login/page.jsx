f290e196cbb9f443b0b7343c1241ea9878c7cc80
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-10-28 19:10:09 +0330

    added useSession, useRouter

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,11 +1,28 @@
+"use client";
+import { useSession, signIn } from "next-auth/react";
 import styles from "./login.module.css";
+import { useRouter } from "next/navigation";
 
 const LogIn = () => {
+  const { status } = useSession();
+
+  const router = useRouter();
+
+  if (status === "loading") {
+    return <div className={styles.loading}>Loading...</div>;
+  }
+
+  if (status === "authenticated") {
+    router.push("/");
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
+        <div className={styles.social} onClick={() => signIn("google")}>
+          Sign in with Google
+        </div>
         <div className={styles.social}>log in with Git Hub</div>
-        <div className={styles.social}>log in with Google</div>
         <div className={styles.social}>log in with LinkedIn</div>
       </div>
     </div>
