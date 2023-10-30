69d0dc1d5fcb8b9cc392b2e99792e2f90279319c
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-10-30 19:44:30 +0330

    modified

--- src/components/authLink/AuthLink.jsx
+++ src/components/authLink/AuthLink.jsx
@@ -1,66 +1,14 @@
-// "use client";
-// import Link from "next/link";
-// import styles from "./authLink.module.css";
-// import { useState } from "react";
-
-// const AuthLink = () => {
-//   const [open, setOpen] = useState(false);
-//   const status = "notauthentication";
-//   return (
-//     <>
-//       {status === "notauthenticatin" ? (
-//         <Link href="/login">LogIn</Link>
-//       ) : (
-//         <>
-//           <Link href="/write">Write</Link>
-//           <span className={styles.link} href="/logout">
-//             LogOut
-//           </span>
-//         </>
-//       )}
-//       <div className={styles.burger}>
-//         <div className={styles.line}></div>
-//         <div className={styles.line}></div>
-//         <div className={styles.line}></div>
-//       </div>
-//       {open && (
-//         <div
-//           className={styles.responsiveMenu}
-//           onClick={() => {
-//             setOpen(!open);
-//           }}
-//         >
-//           <Link href="/">homepage</Link>
-//           <Link href="/">contact</Link>
-//           <Link href="/">about</Link>
-//           {status === "notauthenticatin" ? (
-//             <Link href="/login">LogIn</Link>
-//           ) : (
-//             <>
-//               <Link href="/write">Write</Link>
-//               <span className={styles.link} href="/logout">
-//                 LogOut
-//               </span>
-//             </>
-//           )}
-//         </div>
-//       )}
-//     </>
-//   );
-// };
-
-// export default AuthLink;
-
 "use client";
 import Link from "next/link";
 import styles from "./authLink.module.css";
 import { useState } from "react";
+import { signOut, useSession } from "next-auth/react";
 
 const AuthLink = () => {
   const [open, setOpen] = useState(false);
 
-  const status = "notauthentication";
-
+  const { status } = useSession();
+  // const status = "unauthenticated";
   return (
     <>
       {status === "unauthenticated" ? (
@@ -72,7 +20,9 @@ const AuthLink = () => {
           <Link href="/write" className={styles.link}>
             Write
           </Link>
-          <span className={styles.link}>Logout</span>
+          <span className={styles.link} onClick={signOut}>
+            Logout
+          </span>
         </>
       )}
       <div className={styles.burger} onClick={() => setOpen(!open)}>
