da6910d0ca0d9a91036aca7a1d9f1eb43ff66f57
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-10-30 19:42:09 +0330

    modified

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,46 @@
+// import Navbar from "@/components/navbar/Navbar";
+// import "./globals.css";
+// import { Inter } from "next/font/google";
+// import Footer from "@/components/footer/Footer";
+// import { ThemeContextProvider } from "@/context/ThemeContext";
+// import ThemeProvider from "@/provider/ThemeProvider";
+// import AuthProvider from "@/provider/authProvider";
+
+// const inter = Inter({ subsets: ["latin"] });
+
+// export const metadata = {
+//   title: "Lama Dev Blog App",
+//   description: "The best blog app!"
+// };
+
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="en">
+//       <body className={inter.className}>
+//         <AuthProvider>
+//           <ThemeContextProvider>
+//             <ThemeProvider>
+//               <div className="container">
+//                 <div className="wrapper">
+//                   <Navbar />
+//                   {children}
+//                   <Footer />
+//                 </div>
+//               </div>
+//             </ThemeProvider>
+//           </ThemeContextProvider>
+//         </AuthProvider>
+//       </body>
+//     </html>
+//   );
+// }
 import Navbar from "@/components/navbar/Navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Footer from "@/components/footer/Footer";
 import { ThemeContextProvider } from "@/context/ThemeContext";
 import ThemeProvider from "@/provider/ThemeProvider";
-import AuthProvider from "@/provider/authProvider";
+import AuthProvider from "@/provider/AuthProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
