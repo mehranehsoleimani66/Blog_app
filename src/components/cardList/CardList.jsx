600e7eb1627185f3e9a052cda83fd00cb51a952f
mehraneh <139059903+mehranehsoleimani66@users.noreply.github.com>
2023-11-11 21:33:24 +0330

    modified

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./cardlist.module.css";
+import styles from "./CardList.module.css";
 import Pagination from "../pagination/Pagination";
 import Image from "next/image";
 import Card from "../card/Card";
